cac9155f8ed9e820e2845fb4023f419aa17efdd5
ThachQuangLe <lequangthachbk@gmail.com>
2020-01-09 11:54:25 +0700

    Add Footer

--- Client/src/core/Layout.js
+++ Client/src/core/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Menu from "./Menu";
+import Footer from "./Footer";
 import "../styles.css";
 
 const Layout = ({
@@ -15,6 +16,8 @@ const Layout = ({
             <p className="lead">{description}</p>
         </div>
         <div className={className}>{children}</div>
+        <hr></hr>
+        <Footer></Footer>
     </div>
 );
 
