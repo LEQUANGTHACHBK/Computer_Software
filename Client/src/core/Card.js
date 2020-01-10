135defdc8161486b2c085d4039ecc52bd2b868f1
Hoang <hoangnguyen.ie12@gmail.com>
2020-01-10 11:50:14 +0700

    add commas

--- Client/src/core/Card.js
+++ Client/src/core/Card.js
@@ -3,6 +3,7 @@ import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment';
 import { addItem, updateItem, removeItem } from './cartHelpers';
+import{numberWithComma} from "./apiCore";
 
 const Card = ({
   product,
@@ -104,7 +105,7 @@ const Card = ({
         {shouldRedirect(redirect)}
         <ShowImage item={product} url="product"/>
         <p className="card-p nameproduct  mt-2">{product.description.substring(0, 35)}... </p>
-        <p className="card-p black-10">Giá: {product.price} VND</p>
+        <p className="card-p black-10">Giá: {numberWithComma(product.price)} VND</p>
 
         {showViewButton(showViewProductButton)}
 
